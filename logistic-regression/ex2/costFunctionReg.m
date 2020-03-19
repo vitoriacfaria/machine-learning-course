deff537239d2880e47a58bdf6feee00dab1e882b
Vitoria Cristiane <vicristianecf@gmail.com>
2020-03-19 15:34:14 -0300

    feat(logistic-regression): adiciona cost function com regularizacao

--- logistic-regression/ex2/costFunctionReg.m
+++ logistic-regression/ex2/costFunctionReg.m
@@ -17,10 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+g_function = sigmoid(X*theta);
+regularization = (lambda/(2*m))*sum(theta(2:length(theta)).^2);
+J = -(1/m)*(y'*log(g_function)+(1-y)'*log(1-g_function))+regularization;
 
-
-
-
+prediction_error = g_function - y;
+grad_regularization = [0; (lambda/m)*theta(2:length(theta))];
+grad = (1/m)*(X'*prediction_error)+grad_regularization;
 
 % =============================================================
 
